26009dcbe713cea29799a21097a495ca4fc1d90b
Mic <miscelo@tutanota.com>
2022-03-19 02:28:55 +0100

    Add username.py file

---
+++ username.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def checkusername(username):
+        assert type(username) == str, "ERROR: Username must be letters or numbers."
+        #assert len(username) >=2, "ERROR: Username must be have at least 1 character."
+
+
+def createuser():
+    while(True):
+        try:
+            username = input("Username: ")
+            checkusername(username)
+            break
+        except Exception as e:
+            print("{}: Please choose another username!".format(e))
+    print("Your username is {}!".format(username))
+
+if __name__ == "__main__":
+    createuser()
+
